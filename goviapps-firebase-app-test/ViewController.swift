56dc61a6cf25b02534f26659f1f7274555ce68be
Maciej Serwicki <macserwicki@gmail.com>
2016-01-19 13:15:56 -0600

    Initial Commit

---
+++ goviapps-firebase-app-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goviapps-firebase-app-test
+//
+//  Created by Maciej Serwicki on 1/19/16.
+//  Copyright © 2016 Maciej Serwicki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
